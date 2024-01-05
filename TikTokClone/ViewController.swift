286aede3a1996052fe487ed14bad67b7a868040d
seanli01 <seanmingli2001@gmail.com>
2024-01-05 01:07:43 -0500

    Initial Commit

---
+++ TikTokClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTokClone
+//
+//  Created by Sean Li on 2024-01-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
